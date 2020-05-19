852d85bc8b7a4fe22f607965060f4679c98faf18
Atle Nordland <atle.nordland@nav.no>
2020-05-19 12:55:44 +0200

    Initial commit, copy from dagpenger-funksjonelle-tester

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    application
+    kotlin("jvm") version Kotlin.version
+    id(Spotless.spotless) version Spotless.version
+    id(Shadow.shadow) version Shadow.version
+    id("com.palantir.git-version") version "0.11.0"
+}
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+}
+
+apply {
+    plugin(Spotless.spotless)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+val gitVersion: groovy.lang.Closure<Any> by extra
+version = gitVersion()
+group = "no.nav.dagpenger"
+
+application {
+    applicationName = "dp-saksbehandling-funksjonelle-tester"
+    mainClassName = "no.nav.dagpenger.cucumber.RunCucumberKt"
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation(Cucumber.java8)
+    implementation(Cucumber.junit)
+
+    implementation(Fuel.fuel) {
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    implementation(Fuel.fuelMoshi)
+
+    implementation(Junit5.api)
+
+    implementation(Konfig.konfig)
+
+    implementation(Moshi.moshi)
+    implementation(Moshi.moshiKotlin) {
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    implementation(Moshi.moshiAdapters)
+
+    runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:${Kotlin.version}")
+}
+
+spotless {
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        target("*.gradle.kts", "buildSrc/**/*.kt*")
+        ktlint()
+    }
+}
---
+++ src/main/resources/features/Saksbehandling.feature
@@ -0,0 +1,7 @@
+#language: no
+Egenskap: Saksbehandling
+
+  Scenario: Flere arbeidsforhold gir manuell behandling
+    Gitt en søker med aktør id "FLERE_ARBEIDSFORHOLD"
+    Når vi skal vurdere søknaden
+    Så må søknaden manuelt behandles
