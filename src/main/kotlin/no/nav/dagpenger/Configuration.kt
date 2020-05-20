48681f67f392e9758e9ff744e41abbbf4ec062a2
bendreas <andreas.nygard.bergh@nav.no>
2020-05-20 12:23:15 +0200

    Try to comment out rapid-config to debug

--- src/main/kotlin/no/nav/dagpenger/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/Configuration.kt
@@ -1,7 +1,6 @@
 package no.nav.dagpenger
 
 import com.natpryce.konfig.* // ktlint-disable no-wildcard-imports
-import java.io.File
 
 private const val TOPIC = "privat-dagpenger-behov-v2"
 
@@ -25,10 +24,8 @@ private val devProperties = ConfigurationMap(
         )
 )
 
-private val optionalFile = ConfigurationProperties.fromOptionalFile(File("/var/run/secrets/nais.io/service_user"))
-
 private fun config() = when (System.getenv("CUCUMBER_ENV") ?: System.getProperty("CUCUMBER_ENV")) {
-    "dev" -> ConfigurationProperties.systemProperties() overriding EnvironmentVariables overriding devProperties overriding optionalFile
+    "dev" -> ConfigurationProperties.systemProperties() overriding EnvironmentVariables overriding devProperties
     else -> {
         ConfigurationProperties.systemProperties() overriding EnvironmentVariables overriding localProperties
     }
@@ -37,14 +34,14 @@ private fun config() = when (System.getenv("CUCUMBER_ENV") ?: System.getProperty
 object Configuration {
     val profile: Profile = config()[Key("profile", stringType)].let { Profile.valueOf(it) }
 
-    val rapidApplication: Map<String, String> = mapOf(
+    /*val rapidApplication: Map<String, String> = mapOf(
             "KAFKA_BOOTSTRAP_SERVERS" to config()[Key("kafka.bootstrap.servers", stringType)],
             "KAFKA_CONSUMER_GROUP_ID" to "dp-saksbehandling-funksjonelle-tester",
             "KAFKA_RAPID_TOPIC" to config()[Key("kafka.topic", stringType)],
             "KAFKA_RESET_POLICY" to config()[Key("kafka.reset.policy", stringType)],
             "NAV_TRUSTSTORE_PATH" to config()[Key("nav.truststore.path", stringType)],
             "NAV_TRUSTSTORE_PASSWORD" to config()[Key("nav.truststore.password", stringType)]
-    )
+    )*/
 }
 
 enum class Profile {
