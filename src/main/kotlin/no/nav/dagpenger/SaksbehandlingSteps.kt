d9db64b84ded62637ef8fcd26441f8fa5e0e584a
bendreas <andreas.nygard.bergh@nav.no>
2020-05-20 10:47:41 +0200

    add dummy steps

--- src/main/kotlin/no/nav/dagpenger/SaksbehandlingSteps.kt
+++ src/main/kotlin/no/nav/dagpenger/SaksbehandlingSteps.kt
@@ -5,14 +5,14 @@ import io.cucumber.java8.No
 class SaksbehandlingSteps : No {
 
     init {
-        Gitt("en søker med aktør id {string}") {
-            aktørId: String -> "abc"
+        Gitt("en søker med aktør id {string}") { aktørId: String ->
+            "abc"
         }
 
         Når("vi skal vurdere søknaden") { }
 
         Så("må søknaden manuelt behandles") {
-           assert(true)
+            assert(true)
         }
     }
 }
