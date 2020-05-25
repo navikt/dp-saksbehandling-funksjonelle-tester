2db0e94674e05286d2f5d73a77daf0cfe6d53519
bendreas <andreas.nygard.bergh@nav.no>
2020-05-25 10:25:27 +0200

    Remove rapid, use old fashioned consumer

--- src/main/kotlin/no/nav/dagpenger/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/Configuration.kt
@@ -34,11 +34,13 @@ private fun config() = when (System.getenv("CUCUMBER_ENV") ?: System.getProperty
 
 object Configuration {
     val profile: Profile = config()[Key("profile", stringType)].let { Profile.valueOf(it) }
+    val bootstrapServers = config()[Key("kafka.bootstrap.servers", stringType)]
+    val topic = config()[Key("kafka.topic", stringType)]
 
     val rapidApplication: Map<String, String> = mapOf(
-            "KAFKA_BOOTSTRAP_SERVERS" to config()[Key("kafka.bootstrap.servers", stringType)],
+            "KAFKA_BOOTSTRAP_SERVERS" to bootstrapServers,
             "KAFKA_CONSUMER_GROUP_ID" to "dp-saksbehandling-funksjonelle-tester",
-            "KAFKA_RAPID_TOPIC" to config()[Key("kafka.topic", stringType)],
+            "KAFKA_RAPID_TOPIC" to topic,
             "KAFKA_RESET_POLICY" to config()[Key("kafka.reset.policy", stringType)],
             "NAV_TRUSTSTORE_PATH" to config()[Key("nav.truststore.path", stringType)],
             "NAV_TRUSTSTORE_PASSWORD" to config()[Key("nav.truststore.password", stringType)]
--- src/main/kotlin/no/nav/dagpenger/SaksbehandlingSteps.kt
+++ src/main/kotlin/no/nav/dagpenger/SaksbehandlingSteps.kt
@@ -7,11 +7,16 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import de.huxhorn.sulky.ulid.ULID
 import io.cucumber.java8.No
 import io.kotest.matchers.shouldBe
+import java.time.Duration
 import java.time.LocalDateTime
+import java.util.Properties
 import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.helse.rapids_rivers.RapidsConnection
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
 
-class SaksbehandlingSteps : No {
-    private lateinit var rapid: Rapid
+class SaksbehandlingSteps() : No {
     private lateinit var søknad: Map<String, String>
 
     private val rapidsConnection = RapidApplication.Builder(
@@ -28,7 +33,7 @@ class SaksbehandlingSteps : No {
     }
 
     init {
-        Gitt("en søker med aktør id {string}") { aktørId: String ->
+        Gitt("en søker med aktørid {string}") { aktørId: String ->
             søknad = mapOf(
                     "@id" to ULID().nextULID(),
                     "@event_name" to "Søknad",
@@ -37,9 +42,6 @@ class SaksbehandlingSteps : No {
                     "aktørId" to aktørId,
                     "behandlingId" to ULID().nextULID()
             )
-
-            rapid = Rapid(rapidsConnection, aktørId)
-            rapidsConnection.start()
         }
 
         Når("vi skal vurdere søknaden") {
@@ -47,8 +49,24 @@ class SaksbehandlingSteps : No {
             Thread.sleep(10000)
         }
 
-        Så("må søknaden manuelt behandles") {
-            rapid.vedtakErEndret shouldBe true
+        Så("må søknaden for aktørid {string} manuelt behandles") { aktørId: String ->
+            val consumer = createConsumer(Configuration.bootstrapServers)
+            consumer.subscribe(listOf(Configuration.topic))
+
+            val records = consumer.poll(Duration.ofSeconds(3L))
+
+            records.map { objectMapper.readTree(it.value()) }
+                    .filter { it["@event_name"].asText() == "vedtak_endret" }
+                    .any { it["aktørId"].asText() == aktørId } shouldBe true
         }
+    }
+
+    private fun createConsumer(brokers: String): Consumer<String, String> {
+        val props = Properties()
+        props["bootstrap.servers"] = brokers
+        props["group.id"] = "dp-saksbehandling-funksjonelle-tester-kodd"
+        props["key.deserializer"] = StringDeserializer::class.java
+        props["value.deserializer"] = StringDeserializer::class.java
+        return KafkaConsumer<String, String>(props)
     }
 }
