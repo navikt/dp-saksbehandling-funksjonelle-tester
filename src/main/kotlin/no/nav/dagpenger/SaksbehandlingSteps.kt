098a3779e9e578ccdb80ad6065d3a9bb9737145f
bendreas <andreas.nygard.bergh@nav.no>
2020-05-29 12:53:55 +0200

    Mark keys as interesting

--- src/main/kotlin/no/nav/dagpenger/SaksbehandlingSteps.kt
+++ src/main/kotlin/no/nav/dagpenger/SaksbehandlingSteps.kt
@@ -7,57 +7,45 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import de.huxhorn.sulky.ulid.ULID
 import io.cucumber.java8.No
 import io.kotest.matchers.ints.shouldBeGreaterThan
+import java.time.Duration
+import java.time.LocalDateTime
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import mu.KotlinLogging
-import no.nav.helse.rapids_rivers.*
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
 import org.awaitility.kotlin.await
-import java.time.Duration
-import java.time.LocalDateTime
 
 private val log = KotlinLogging.logger {}
 
 class SaksbehandlingSteps() : No {
     private lateinit var søknad: Map<String, String>
 
-    companion object {
+    private val objectMapper = jacksonObjectMapper()
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
 
-        private val objectMapper = jacksonObjectMapper()
-                .registerModule(JavaTimeModule())
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-
-        private val messages = mutableListOf<JsonMessage>()
-        private val rapidsConnection = RapidApplication.Builder(
-                RapidApplication.RapidApplicationConfig.fromEnv(Configuration.rapidApplication)
-        ).build()
-                .also {
-                    object : River.PacketListener {
-                        init {
-                            River(it).register(this)
-                        }
-
-                        override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) {
-                            log.info { "packet found" }
-                            messages.add(packet)
-                        }
-
-
-                        override fun onError(problems: MessageProblems, context: RapidsConnection.MessageContext) {
-                            log.error { "Problems!! ->  ${problems}" }
-
-                        }
-
-                        override fun onSevere(error: MessageProblems.MessageException, context: RapidsConnection.MessageContext) {
-                            log.error(error) { "Something bad happen" }
-                        }
+    private val messages = mutableListOf<JsonMessage>()
+    private val rapidsConnection = RapidApplication.Builder(
+            RapidApplication.RapidApplicationConfig.fromEnv(Configuration.rapidApplication)
+    ).build()
+            .also {
+                object : River.PacketListener {
+                    init {
+                        River(it).register(this)
                     }
-                }.also {
-                    GlobalScope.launch { it.start() }
+
+                    override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) {
+                        packet.interestedIn("aktørId", "@event_name")
+                        messages.add(packet)
+                    }
                 }
-    }
-
-
+            }.also {
+                GlobalScope.launch { it.start() }
+            }
 
     fun sendToRapid(behov: Map<*, *>) {
         rapidsConnection.publish(objectMapper.writeValueAsString(behov))
@@ -92,7 +80,6 @@ class SaksbehandlingSteps() : No {
             }
             log.info { "finished" }
             log.info { "messages size: ${messages.size}" }
-
         }
     }
 }
